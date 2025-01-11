b48f51d387389491fbf4bb64c7d32f3ee202fdbb
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-10 21:20:01 -0600

    feat: implement TopicService with CRUD operations
    - Add TopicService to handle creation, listing, retrieval, update, and deletion of topics.
    - Integrate TopicRepository, UserRepository, and CourseRepository for data management.
    - Include validations for topic data before operations.

--- src/main/java/com/alfonsovidrio/forohub/domain/topic/TopicService.java
+++ src/main/java/com/alfonsovidrio/forohub/domain/topic/TopicService.java
@@ -2,22 +2,29 @@ package com.alfonsovidrio.forohub.domain.topic;
 
 import com.alfonsovidrio.forohub.domain.course.Course;
 import com.alfonsovidrio.forohub.domain.course.CourseRepository;
+import com.alfonsovidrio.forohub.domain.topic.validations.Validations;
 import com.alfonsovidrio.forohub.domain.user.User;
 import com.alfonsovidrio.forohub.domain.user.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Service
 public class TopicService {
-    private final TopicRepository topicRepository;
-    private final UserRepository userRepository;
-    private final CourseRepository courseRepository;
+    @Autowired
+    private TopicRepository topicRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private List<Validations> validations;
 
     public TopicService(TopicRepository topicRepository, UserRepository userRepository, CourseRepository courseRepository) {
         this.topicRepository = topicRepository;
@@ -31,6 +38,8 @@ public class TopicService {
         Course course = courseRepository.findById(topicData.courseId())
                 .orElseThrow(() -> new IllegalArgumentException("Invalid course ID"));
 
+        validations.forEach(v -> v.validate(topicData.title(), topicData.message()));
+
         Topic topic = new Topic(topicData);
         topic.setUser(user);
         topic.setCourse(course);
@@ -41,5 +50,21 @@ public class TopicService {
     public Page<GetTopicData> listTopics(Pageable pageable) {
         Page<Topic> topics = topicRepository.findAllByOrderByCreationDateDesc(pageable);
         return topics.map(GetTopicData::new);
+    }
+
+    public Topic getTopic(Long id) {
+        return topicRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid topic ID"));
+    }
+
+    public void updateTopic(@Valid UpdateTopicData updateTopicData) {
+        validations.forEach(v -> v.validate(updateTopicData.title(), updateTopicData.message()));
+        Topic topic = getTopic(updateTopicData.id());
+        topic.update(updateTopicData);
+        topicRepository.save(topic);
+    }
+
+    public void deleteTopic(Topic topic) {
+        topicRepository.delete(topic);
     }
 }
