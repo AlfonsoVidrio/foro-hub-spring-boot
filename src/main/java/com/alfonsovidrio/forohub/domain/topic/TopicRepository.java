163af4b14171e5569d52021210c742c13dca65ca
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-10 21:20:59 -0600

    feat: add TopicRepository for topic data persistence
    - Implement JpaRepository for CRUD operations on Topic entity.
    - Add method to find all topics ordered by creation date in descending order.
    - Include method to check existence of a topic by title and message.

--- src/main/java/com/alfonsovidrio/forohub/domain/topic/TopicRepository.java
+++ src/main/java/com/alfonsovidrio/forohub/domain/topic/TopicRepository.java
@@ -1,9 +1,12 @@
 package com.alfonsovidrio.forohub.domain.topic;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
     Page<Topic> findAllByOrderByCreationDateDesc(Pageable pageable);
+
+    boolean existsByTitleAndMessage(@NotBlank String title, String message);
 }
